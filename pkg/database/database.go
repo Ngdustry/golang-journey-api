d169e995aa7b8a12261c95916747c3f0d8c5b93f
Patrick Ng <ngdustry@gmail.com>
2020-09-29 11:29:01 -0400

    fix: pass db PORT

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -32,7 +32,7 @@ type Task struct {
 var db *gorm.DB
 
 func New() {
-	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v", os.Getenv("HOST"), os.Getenv("USER"), os.Getenv("PW"), os.Getenv("DBNAME"), os.Getenv("DBPORT"))
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=5432", os.Getenv("HOST"), os.Getenv("USER"), os.Getenv("PW"), os.Getenv("DBNAME"))
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to DB")
