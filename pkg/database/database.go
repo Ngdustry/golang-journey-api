890ad2cfb12e04032d30d5c7676e3e1556bc11cf
Patrick Ng <ngdustry@gmail.com>
2021-01-31 20:24:31 -0500

    refactor: removed task and user snippets

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -1,33 +1,12 @@
 package database
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"net/http"
 	"os"
 
-	"github.com/google/uuid"
-	"github.com/gorilla/mux"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
-
-// User struct
-type User struct {
-	gorm.Model
-	ID        uuid.UUID `gorm:"primaryKey" json:"id"`
-	FirstName string    `json:"firstName"`
-}
-
-// Task struct
-type Task struct {
-	gorm.Model
-	ID     uuid.UUID `gorm:"primaryKey" json:"id"`
-	Text   string    `json:"text"`
-	Status string    `json:"status"`
-	User   User      `gorm:"embedded" json:"user"`
-}
 
 var db *gorm.DB
 
@@ -44,55 +23,4 @@ func New() {
 	// Migrate schema
 	db.AutoMigrate(&Task{})
 	db.AutoMigrate(&User{})
-}
-
-// FindAllTasks will get all tasks.
-func FindAllTasks() (res []Task) {
-	var tasks []Task
-	db.Find(&tasks)
-
-	return tasks
-}
-
-// FindOneTask will get specific task.
-func FindOneTask(id string) (Task, error) {
-	var task Task
-	var err error
-	result := db.First(&task, id)
-
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) == true {
-		err = errors.New("No task found")
-	}
-
-	return task, err
-}
-
-// CreateNewTask will create a new task.
-func CreateNewTask(r *http.Request) (id uuid.UUID, err error) {
-	var task Task
-
-	json.NewDecoder(r.Body).Decode(&task)
-
-	newID := uuid.New()
-	task.ID = newID
-
-	result := db.Create(&task)
-
-	return task.ID, result.Error
-}
-
-// UpdateOneTask will update a specific task by ID.
-func UpdateOneTask(r *http.Request) (err error) {
-	var updatedTask Task
-	json.NewDecoder(r.Body).Decode(&updatedTask)
-	params := mux.Vars(r)
-
-	result := db.Model(&Task{}).Where("id = ?", params["id"]).Updates(map[string]interface{}{"text": updatedTask.Text, "status": updatedTask.Status})
-
-	return result.Error
-}
-
-//DeleteOneTask will delete a specific task by ID.
-func DeleteOneTask(id string) {
-	db.Where("id = ?", id).Delete(&Task{})
 }
