62791bda9192484ce57dc4efc900031b79358839
Patrick Ng <ngdustry@gmail.com>
2021-02-01 00:12:22 -0500

    feat: added create new user db operation

--- pkg/database/user.go
+++ pkg/database/user.go
@@ -1,1 +1,17 @@
 package database
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// CreateNewUser will create a new user.
+func CreateNewUser(r *http.Request) (id string, err error) {
+	var user User
+
+	json.NewDecoder(r.Body).Decode(&user)
+
+	result := db.Create(&user)
+
+	return user.ID, result.Error
+}
